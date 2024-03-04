c6bc0477b4436b93968c3c149459aa134f4b8e7e
lardaat <kodymskiins@gmail.com>
2024-03-04 18:32:44 +0300

    Solution

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,29 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld beanHello =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(beanHello.getMessage());
+
+        Cat beanCat =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat.getMessage());
+
+        Cat beanCat1 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat1.getMessage());
+
+        boolean helloWorldEquals = bean.equals(beanHello);
+        boolean catEquals = beanCat.equals(beanCat1);
+
+
+        System.out.println("HelloWorld equals: " + helloWorldEquals);
+        System.out.println("Cat equals: " + catEquals);
+
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,14 @@
+import org.springframework.context.annotation.Scope;
+
+
+public class Cat {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
