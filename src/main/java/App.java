1ef881b150c3e5e54bd33ba318925e8b73997ca9
lardaat <kodymskiins@gmail.com>
2024-03-04 18:53:44 +0300

    Solution

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,13 +6,13 @@ public class App {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
 
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
-
         HelloWorld beanHello =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(beanHello.getMessage());
+
+        HelloWorld beanHello1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(beanHello1.getMessage());
 
         Cat beanCat =
                 (Cat) applicationContext.getBean("cat");
@@ -22,12 +22,13 @@ public class App {
                 (Cat) applicationContext.getBean("cat");
         System.out.println(beanCat1.getMessage());
 
-        boolean helloWorldEquals = bean.equals(beanHello);
+        System.out.println("HelloWorld equals: " + (beanHello==beanHello1));
+        System.out.println("Cat equals: " + (beanCat==beanCat1));
+
+        /*boolean helloWorldEquals = beanHello.equals(beanHello1);
         boolean catEquals = beanCat.equals(beanCat1);
-
-
         System.out.println("HelloWorld equals: " + helloWorldEquals);
-        System.out.println("Cat equals: " + catEquals);
+        System.out.println("Cat equals: " + catEquals);*/
 
     }
 }
